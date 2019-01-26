0df56aa8ac1a30dc23c5bc6981ab9cacf38a6727
Michael Trew <mtrew2015@gmail.com>
2019-01-26 13:12:39 -0500

    added dbHelpers file

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,18 @@
 const knex = require('knex');
-
 const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development)
 
-module.exports = knex(knexConfig.development);
+module.exports = {
+  insertUser: (user) => {
+    return db('users').insert(user);
+  },
+  findByUsername: (username) => {
+    return db('users').where('username', username).first();
+  },
+  findByID: (id) => {
+    return db('users').where('id', id).first();
+  },
+  getUsers: (users) => {
+    return db('users').select('username', 'department', 'id');
+  }
+}
